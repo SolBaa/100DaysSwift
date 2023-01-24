f747d69cda3288f6d2a760c849732673215e5338
Sol <solbattaglia@gmail.com>
2023-01-24 13:57:21 -0300

    final twicks

--- second_project/second_project/ViewController.swift
+++ second_project/second_project/ViewController.swift
@@ -28,9 +28,9 @@ class ViewController: UIViewController {
         
     
 
-        askQuestion()
+        askQuestion(action: nil)
     }
-    func askQuestion(){
+    func askQuestion(action: UIAlertAction! = nil){
         countries.shuffle()
         correctAnswer =  Int.random(in: 0..<countries.count)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
@@ -40,5 +40,20 @@ class ViewController: UIViewController {
         
     }
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score += 1
+        }else{
+            title = "Wrong"
+            score -= 1
+        }
+        let ac = UIAlertController(title: title, message: "Your Score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac,animated: true)
+    }
 }
 
